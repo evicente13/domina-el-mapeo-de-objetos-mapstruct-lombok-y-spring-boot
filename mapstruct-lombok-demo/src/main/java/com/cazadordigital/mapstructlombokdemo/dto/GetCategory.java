37bf8e0a6afcd048f529a3ca858aa19f370db326
Eduardo Vicente Marroquín <eduardoartemis@gmail.com>
2023-07-05 22:53:28 -0500

    Video 09 - 05/07/2023 22:53

---
+++ mapstruct-lombok-demo/src/main/java/com/cazadordigital/mapstructlombokdemo/dto/GetCategory.java
@@ -0,0 +1,15 @@
+package com.cazadordigital.mapstructlombokdemo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GetCategory {
+    private long id;
+    private String name;
+}
